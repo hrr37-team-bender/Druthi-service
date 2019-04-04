ef95fd9dfbc8b23c611ae3d6d0374f928345c8d1
Druthi <polisettydruthi.k@gmail.com>
2019-04-03 23:39:04 -0700

    Fixed Css and added link to display image

--- client/src/components/DisplayImage.jsx
+++ client/src/components/DisplayImage.jsx
@@ -4,16 +4,19 @@ import styled from 'styled-components';
 const Box = styled.div`
   margin-left: 10px;
   width: 31%;
+  height: 300px;
 `;
 const Img = styled.img`
   width: 100%;
+  height: -webkit-fill-available;
 `;
 
 var DisplayImage = ({ image }) => {
   return (
-    <Box>
+    <a href={image.image_url}><Box>
       <Img src={image.image_url}/>
     </Box>
+    </a>
   );
 };
 
