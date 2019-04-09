e3d42e3048e252c14548334687aebc032de98d4b
Druthi <polisettydruthi.k@gmail.com>
2019-04-08 18:03:54 -0700

    Integrated s3 bucket to host the images

--- server/db/dummy-data.js
+++ server/db/dummy-data.js
@@ -1,21 +1,59 @@
 
 const faker = require('faker');
 var fs = require('fs');
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({
+  accessKeyId: 'AKIAIQVE3IXHOLOREYQA',
+  secretAccessKey: 'B1NBp+CYtU/ecI1t1E8cyaceb+jA1UaCDoEmTxm+'
+});
 
+// Create S3 service object
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-var dummyData= [];
-
-
-var generateData  = () => {
-  for(let i = 1; i<=100; i++) {
-    dummyData.push({
-      id:i,
-      image_url: faker.image.cats()
-    });
-  }
-  var json = JSON.stringify(dummyData);
-  fs.writeFile('data.json', json, 'utf8', ()=>{console.log("done")});
+// Create the parameters for calling listObjects
+var bucketParams = {
+  Bucket : 'deepfryd',
 };
+
+// Call S3 to obtain a list of the objects in the bucket
+s3.listObjects(bucketParams, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+    const baseUrl = 'https://s3.ap-south-1.amazonaws.com/deepfryd/';
+  }
+});
+
+
+var dummyData = [];
+
+
+var generateData = () => {
+  s3.listObjects(bucketParams, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+      const baseUrl = 'https://s3.ap-south-1.amazonaws.com/deepfryd/';
+      data.Contents.forEach((image, index) => {
+        dummyData.push({
+          id: index,
+          'image_url': `${baseUrl}/${image.Key}`
+        });
+      });
+
+      var json = JSON.stringify(dummyData);
+      fs.writeFile('data.json', json, 'utf8', ()=> {
+        console.log('done');
+      });
+    }
+  });
+};
+
+
 
 
 
